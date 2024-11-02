e90bbd19e1bfef564b1f56817d45f54094a616f2
v1sh-afk <vishalprakaash03@gmail.com>
2024-11-03 00:44:45 +0530

    v3 updated frontend

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
 import './Header.css';
+import dramaLogo from './assets/drama.png'; 
 
 const Header = () => {
   return (
     <div className='but1 makecenter'>
-      <h2>DramaShield</h2>
+      <div className='logo-container'>
+        <img src={dramaLogo} alt='DramaShield Logo' className='logo' />
+        <h2>DramaShield</h2>
+      </div>
       <button>Get Extension</button>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
